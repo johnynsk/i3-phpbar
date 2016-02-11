4a13735267db107736ab084166885f51ab3c526b
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-02-11 09:50:16 +0600

    Jira timespent caching

--- bootstrap.php
+++ bootstrap.php
@@ -4,10 +4,15 @@
  *
  * @param string $className
  */
-function __autoload($className)
-{
-    $fileName = preg_replace('#_#', '/', $className);
-    require_once __dir__ . '/' . $fileName . '.php';
-}
+spl_autoload_register(function ($className) {
+        $fileName = $className;
+        $fileName = preg_replace('/_/', '/', $fileName);
+        $fileName = preg_replace('/\\\\/', '/', $fileName);
+        $fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . '.php';
+        if (file_exists($fileName)) {
+            return require $fileName;
+        }
+    }
+);
 
 $config = require 'config.php';
